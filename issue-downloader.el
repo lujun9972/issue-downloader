dd628c5227f333466f4def41ddcc2c67abdeb3fe
lujun9972 <lujun9972@gmail.com>
2016-04-12 17:43:41 +0800

    first commit

---
+++ issue-downloader.el
@@ -0,0 +1,39 @@
+(require 'gh)
+(defgroup issue-downloader nil
+  "download posts in github issue based blog")
+
+(defcustom issue-downloader-base-dir "/tmp"
+  ""
+  :group 'issue-downloader)
+
+(defcustom issue-downloader-repos nil
+  "specify blog repos. It should be a list of \"USER/REPO\" or (\"USER/REPO\" . download-directory)"
+  :group 'issue-downloader)
+
+(defun issue-downloader-download (user/repo &optional dir since)
+  "Download issues of USER/REPO into DIR. If SINCE is specified (should be a integer), means only download issues newer than it"
+  (unless (file-exists-p dir)
+    (make-directory dir t))
+  (let* ((since (or since 0))
+         (user/repo-list (split-string user/repo "/"))
+         (user (nth 0 user/repo-list))
+         (repo (nth 1 user/repo-list))
+         (dir (or dir (format "%s%s-%s" (file-name-as-directory issue-downloader-base-dir user repo))))
+         (api (gh-issues-api "api"))
+         (issues (oref (gh-issues-issue-list api user repo) data))
+         (valid-issues (remove-if-not (lambda (issue)
+                                        (> (oref issue number) since))
+                                      issues)))
+    (dolist (issue (reverse valid-issues))
+      (let ((title (oref issue title))
+            (body (oref issue body)))
+        (with-temp-file (format "%s/%s.md" dir title)
+          (insert body))))))
+
+;; (issue-downloader-download "lujun9972/blog" "/tmp/1")
+
+;;;###autoload
+(defun issue-downloader ()
+  (mapc (lambda (x)
+          (apply #'issue-downloader-download x))
+        (issue-downloader-repos)))
