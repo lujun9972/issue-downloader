48cd6149d89afb00e153423866aaf8169c248a38
lujun9972 <lujun9972@gmail.com>
2016-04-19 12:45:13 +0800

    fix bug about dir

--- issue-downloader.el
+++ issue-downloader.el
@@ -16,12 +16,10 @@
 
 (defun issue-downloader-download (user/repo &optional dir since)
   "Download issues of USER/REPO into DIR. If SINCE is specified (should be a integer), means only download issues newer than it"
-  (unless (file-exists-p dir)
-    (make-directory dir t))
   (let* ((user/repo-list (split-string user/repo "/"))
          (user (nth 0 user/repo-list))
          (repo (nth 1 user/repo-list))
-         (dir (or dir (format "%s%s-%s" (file-name-as-directory issue-downloader-base-dir user repo))))
+         (dir (or dir (format "%s%s-%s" (file-name-as-directory issue-downloader-base-dir) user repo)))
          (since (cond ((eq t since)
                        (issue--count-file-in-dir dir))
                       ((stringp since)
@@ -34,13 +32,15 @@
          (valid-issues (remove-if-not (lambda (issue)
                                         (> (oref issue number) since))
                                       issues)))
+    (unless (file-exists-p dir)
+      (make-directory dir t))
     (dolist (issue (reverse valid-issues))
       (let ((title (oref issue title))
             (body (oref issue body)))
         (with-temp-file (format "%s/%s.md" dir title)
           (insert body))))))
 
-;; (issue-downloader-download "lujun9972/blog" "/tmp/1" t)
+;; (issue-downloader-download "xufei/blog")
 
 ;;;###autoload
 (defun issue-downloader ()
