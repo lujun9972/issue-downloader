06cb565bb70f68d379bcb4a787fb5012fc5de042
lujun9972 <lujun9972@gmail.com>
2016-04-20 14:39:59 +0800

    add package comment

--- issue-downloader.el
+++ issue-downloader.el
@@ -1,3 +1,44 @@
+;;; issue-downloader.el --- download github issue
+
+;; Copyright (C) 2004-2016 Free Software Foundation, Inc.
+
+;; Author: DarkSun <lujun9972@gmail.com>
+;; Created: 2016-03-24
+;; Version: 0.1
+;; Keywords: convenience, github, org
+;; Package-Requires: ((org "8.0") (emacs "24.4") (ox-gfm "0.1") (gh "0.1"))
+;; URL: https://github.com/lujun9972/issue-downloader
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; issue-downloader's code can be found here:
+;;   http://github.com/lujun9972/issue-downloader
+
+;;; Commentary:
+
+;; issue-downloader is a little tool that used to download github issues
+
+;; Quick start:
+
+;; 1. specify ~issue-downloader-repos~ which specifies which repos to download issues and where to store those issues
+;; 2. execute =M-x issue-downloader=
+
+;;; Code:
 (require 'gh)
 (defgroup issue-downloader nil
   "download posts in github issue based blog")
@@ -7,7 +48,7 @@
   :group 'issue-downloader)
 
 (defcustom issue-downloader-repos nil
-  "specify blog repos. It should be a list of \"USER/REPO\" or (\"USER/REPO\" download-directory)"
+  "specify blog repos. It should be a list of (\"USER/REPO\") or (\"USER/REPO\" download-directory)"
   :group 'issue-downloader)
 
 (defun issue--count-file-in-dir (dir)
@@ -50,3 +91,5 @@
   (mapc (lambda (x)
           (apply #'issue-downloader-download x))
         (issue-downloader-repos)))
+
+;;; org2issue.el ends here
